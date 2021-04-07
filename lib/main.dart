8b0c5fd7186c8e4ead4a766a7a20217eb100ade1
Aryan Agarwal <aryankush025@gmail.com>
2021-04-07 23:06:34 +0530

    Global observer added

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart' as DotEnv;
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import './app/app.dart';
+import './bloc/global_bloc_observer.dart';
 
 Future main() async {
   await DotEnv.load(fileName: ".env");
+
+  Bloc.observer = GlobalBlocObserver();
 
   runApp(MyApp());
 }
