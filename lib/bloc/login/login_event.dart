04dea70bc2215a2ff13e2c4e0d7383b919a452cf
Aryan Agarwal <aryankush025@gmail.com>
2021-04-08 01:09:05 +0530

    Login bloc added

---
+++ lib/bloc/login/login_event.dart
@@ -0,0 +1,30 @@
+import 'package:equatable/equatable.dart';
+
+abstract class LoginEvent extends Equatable {
+  const LoginEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+class LoginEmailChanged extends LoginEvent {
+  final String email;
+
+  const LoginEmailChanged(this.email);
+
+  @override
+  List<Object> get props => [email];
+}
+
+class LoginPasswordChanged extends LoginEvent {
+  final String password;
+
+  const LoginPasswordChanged(this.password);
+
+  @override
+  List<Object> get props => [password];
+}
+
+class LoginSubmitted extends LoginEvent {
+  const LoginSubmitted();
+}
