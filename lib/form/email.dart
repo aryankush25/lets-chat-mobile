d417417ac36e815b3023099dde65af3670e36d78
Aryan Agarwal <aryankush025@gmail.com>
2021-04-08 01:09:16 +0530

    form fields added

---
+++ lib/form/email.dart
@@ -0,0 +1,17 @@
+import 'package:formz/formz.dart';
+
+enum EmailValidationError { empty }
+
+class Email extends FormzInput<String, EmailValidationError> {
+  const Email.pure() : super.pure('');
+  const Email.dirty([String value = '']) : super.dirty(value);
+
+  @override
+  EmailValidationError validator(String value) {
+    if (value.isEmpty) {
+      return EmailValidationError.empty;
+    }
+
+    return null;
+  }
+}
---
+++ lib/form/password.dart
@@ -0,0 +1,17 @@
+import 'package:formz/formz.dart';
+
+enum PasswordValidationError { empty }
+
+class Password extends FormzInput<String, PasswordValidationError> {
+  const Password.pure() : super.pure('');
+  const Password.dirty([String value = '']) : super.dirty(value);
+
+  @override
+  PasswordValidationError validator(String value) {
+    if (value.isEmpty) {
+      return PasswordValidationError.empty;
+    }
+
+    return null;
+  }
+}
